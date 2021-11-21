e0c142ae6810d83d1d099769712da9947c1a873b
ShubhraS0 <94050422+ShubhraS0@users.noreply.github.com>
2021-11-21 15:19:40 +0530

    Add files via upload

--- src/Assignments/JA3_SimpleCalculator.java
+++ src/Assignments/JA3_SimpleCalculator.java
@@ -5,6 +5,7 @@
         4. sub
         Note: Program should take continuous input on what operation to perform from user. Should take inputs required for operation and print output at every operation.
  */
+//added break after each case
 package Assignments;
 
 import java.util.Scanner;
@@ -33,18 +34,22 @@ public class JA3_SimpleCalculator {
             case 1 -> {
                 float Add = firstNum + secondNum;
                 System.out.println("Addition of the entered values " + firstNum + " and " + secondNum + " = " + Add);
+                break;
             }
             case 2 -> {
                 float Multiply = firstNum * secondNum;
                 System.out.println("Multiplication of the entered values " + firstNum + " and " + secondNum + " = " + Multiply);
+                break;
             }
             case 3 -> {
                 float Divide = firstNum / secondNum;
                 System.out.println("Division of the entered values " + firstNum + " and " + secondNum + " = " + Divide);
+                break;
             }
             case 4 -> {
                 float Subtract = firstNum - secondNum;
                 System.out.println("Subtraction of the entered values " + firstNum + " and " + secondNum + " = " + Subtract);
+                break;
             }
             default -> System.out.println("Invalid Entry");
         }
